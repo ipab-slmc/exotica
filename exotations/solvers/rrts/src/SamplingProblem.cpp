1fe7f56c8d7187294f26a62c9130dba3c6d52682
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-04-17 12:10:05 +0100

    RRTs solver

---
+++ exotations/solvers/rrts/src/SamplingProblem.cpp
@@ -0,0 +1,29 @@
+/*
+ * SamplingProblem.cpp
+ *
+ *  Created on: 18 Mar 2015
+ *      Author: yiming
+ */
+
+#include "RRTs/SamplingProblem.h"
+
+namespace exotica
+{
+	SamplingProblem::SamplingProblem()
+	{
+		//TODO
+	}
+
+	SamplingProblem::~SamplingProblem()
+	{
+		//TODO
+	}
+
+	EReturn SamplingProblem::initDerived(tinyxml2::XMLHandle & handle)
+	{
+
+		return SUCCESS;
+	}
+}
+
+

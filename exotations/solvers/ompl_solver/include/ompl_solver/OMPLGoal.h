3a65164e85deb91faa0dbb13222cf80dbb2e1d06
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-04-17 12:09:52 +0100

    OMPL Solver

---
+++ exotations/solvers/ompl_solver/include/ompl_solver/OMPLGoal.h
@@ -0,0 +1,34 @@
+/*
+ * OMPLGoal.h
+ *
+ *  Created on: 14 Jul 2014
+ *      Author: s0972326
+ */
+
+#ifndef OMPLGOAL_H_
+#define OMPLGOAL_H_
+
+#include <ompl/base/Goal.h>
+#include "ompl_solver/OMPLProblem.h"
+#include "ompl_solver/OMPLStateSpace.h"
+
+namespace exotica
+{
+
+	class OMPLGoal : public ompl::base::Goal
+	{
+		public:
+			OMPLGoal (const ompl::base::SpaceInformationPtr &si, OMPLProblem_ptr prob);
+			virtual
+			~OMPLGoal ();
+
+			 virtual bool isSatisfied(const ompl::base::State *st) const;
+			 virtual bool isSatisfied(const ompl::base::State *st, double *distance) const;
+		private:
+			 OMPLProblem_ptr prob_;
+			 boost::shared_ptr<OMPLStateSpace> state_space_;
+	};
+
+} /* namespace exotica */
+
+#endif /* OMPLGOAL_H_ */

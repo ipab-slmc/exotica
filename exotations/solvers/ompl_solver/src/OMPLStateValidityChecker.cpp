101a96228b390cc81feee80aa1f3af91c1bc949f
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-04-17 12:09:52 +0100

    OMPL Solver

---
+++ exotations/solvers/ompl_solver/src/OMPLStateValidityChecker.cpp
@@ -0,0 +1,62 @@
+/*
+ * OMPLStateValidityChecker.cpp
+ *
+ *  Created on: 9 Jul 2014
+ *      Author: s0972326
+ */
+
+#include "ompl_solver/OMPLStateValidityChecker.h"
+
+namespace exotica
+{
+
+	OMPLStateValidityChecker::OMPLStateValidityChecker(exotica::OMPLsolver* sol) :
+					sol_(sol),
+					ompl::base::StateValidityChecker(sol->getOMPLSimpleSetup()->getSpaceInformation()),
+					prob_(sol->getProblem())
+	{
+	}
+
+	OMPLStateValidityChecker::~OMPLStateValidityChecker()
+	{
+	}
+
+	bool OMPLStateValidityChecker::isValid(const ompl::base::State *state) const
+	{
+		double tmp;
+		return isValid(state, tmp);
+	}
+
+	bool OMPLStateValidityChecker::isValid(const ompl::base::State *state, double &dist) const
+	{
+		Eigen::VectorXd q(prob_->getSpaceDim());
+		sol_->getOMPLStateSpace()->copyFromOMPLState(state, q);
+		{
+			boost::mutex::scoped_lock lock(prob_->getLock());
+//			prob_->update(q, 0);
+
+// TODO: Implement this
+// Constraints
+// State rejection (e.g. collisions)
+			for (auto & it : prob_->scenes_)
+			{
+				it.second->getCollisionScene()->update(q);
+				if (!it.second->getCollisionScene()->isStateValid())
+				{
+					dist = -1;
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	double OMPLStateValidityChecker::clearance(const ompl::base::State *state) const
+	{
+		double tmp;
+		isValid(state, tmp);
+		return tmp;
+	}
+
+} /* namespace exotica */

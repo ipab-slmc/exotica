f1b7e5742427de769199b62057d243dac4576e40
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-05-14 17:42:08 +0100

    Moved examples

---
+++ examples/tasks/com_demo/include/com_demo/node.h
@@ -0,0 +1,21 @@
+#ifndef IK_SOLVER_DEMO_NODE_H
+#define IK_SOLVER_DEMO_NODE_H
+
+#include "ik_solver/ik_solver.h"
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <exotica/Initialiser.h>
+#include <sensor_msgs/JointState.h>
+
+class IKSolverDemoNode
+{
+public:
+    IKSolverDemoNode();
+private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nhg_;
+    std::string resource_path_;
+    ros::Publisher jointStatePublisher_;
+};
+
+#endif // IK_SOLVER_DEMO_NODE_H

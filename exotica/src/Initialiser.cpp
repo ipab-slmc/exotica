565e2d4066d09ee05bff89c22a298fafe9e479e0
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-05-25 17:20:43 +0100

    Initialise multiple solvers/problems

--- exotica/src/Initialiser.cpp
+++ exotica/src/Initialiser.cpp
@@ -13,7 +13,24 @@ namespace exotica
 	EReturn Initialiser::initialise(const std::string & file_name, Server_ptr & server,
 			MotionSolver_ptr & solver, PlanningProblem_ptr & problem)
 	{
-		return initialise(file_name, server, solver, problem, std::string(), std::string());
+        std::vector<std::string> probs, sols;
+        if(ok(Initialiser::listSolversAndProblems(file_name, probs, sols)))
+        {
+            if(probs.size()>0&&sols.size()>0)
+            {
+                return initialise(file_name, server, solver, problem, probs[0], sols[0]);
+            }
+            else
+            {
+                INDICATE_FAILURE;
+                return FAILURE;
+            }
+        }
+        else
+        {
+            INDICATE_FAILURE;
+            return FAILURE;
+        }
 	}
 
     EReturn Initialiser::initialiseProblemJSON(PlanningProblem_ptr problem, const std::string& constraints)
@@ -151,163 +168,232 @@ namespace exotica
 		return ret_val;
 	}
 
+    EReturn Initialiser::initialise(tinyxml2::XMLHandle root_handle, Server_ptr & server)
+    {
+        server = Server::Instance();
+        tinyxml2::XMLHandle server_handle(root_handle.FirstChildElement("Server"));
+        if (server_handle.ToElement())
+        {
+            if (ok(server->initialise(server_handle)))
+            {
+                return SUCCESS;
+            }
+            else
+            {
+                INDICATE_FAILURE
+                return FAILURE;
+            }
+        }
+        else
+        {
+            ERROR("EXOTica Server element is missing in the xml");
+            INDICATE_FAILURE
+            return FAILURE;
+        }
+    }
+
+    EReturn Initialiser::initialise(tinyxml2::XMLHandle root_handle, PlanningProblem_ptr & problem, const std::string & problem_name, Server_ptr & server)
+    {
+        tinyxml2::XMLHandle problem_handle(root_handle.FirstChildElement());
+        while (problem_handle.ToElement())
+        {
+            const char* atr = problem_handle.ToElement()->Attribute("name");
+            if (atr)
+            {
+                if (std::string(atr).compare(problem_name) == 0)
+                {
+                    if (ok(PlanningProblem_fac::Instance().createObject(problem, problem_handle, server)))
+                    {
+                        return SUCCESS;
+                    }
+                    else
+                    {
+                        INDICATE_FAILURE;
+                        return FAILURE;
+                    }
+                }
+                else
+                {
+                    problem_handle = problem_handle.NextSibling();
+                    continue;
+                }
+            }
+            else
+            {
+                problem_handle = problem_handle.NextSibling();
+                ERROR("Element name was not specified!");
+                continue;
+            }
+        }
+
+        ERROR("File does not contain the '"<<problem_name<<"' problem definition.");
+        return FAILURE;
+    }
+
+    EReturn Initialiser::initialise(tinyxml2::XMLHandle root_handle, MotionSolver_ptr & solver, const std::string & solver_name, Server_ptr & server)
+    {
+        tinyxml2::XMLHandle solver_handle(root_handle.FirstChildElement());
+        while (solver_handle.ToElement())
+        {
+            if (!solver_name.empty())
+            {
+                const char* atr = solver_handle.ToElement()->Attribute("name");
+                if (atr)
+                {
+                    if (std::string(atr).compare(solver_name) == 0)
+                    {
+                        if (ok(MotionSolver_fac::Instance().createObject(solver, solver_handle, server)))
+                        {
+                            return SUCCESS;
+                        }
+                        else
+                        {
+                            INDICATE_FAILURE;
+                            return FAILURE;
+                        }
+                    }
+                    else
+                    {
+                        solver_handle = solver_handle.NextSibling();
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    solver_handle = solver_handle.NextSibling();
+                    ERROR("Element name was not specified!");
+                    continue;
+                }
+            }
+
+        }
+
+        ERROR("File does not contain the '"<<solver_name<<"' solver.");
+        return FAILURE;
+    }
+
 	EReturn Initialiser::initialise(const std::string & file_name, Server_ptr & server,
 			MotionSolver_ptr & solver, PlanningProblem_ptr & problem,
 			const std::string & problem_name, const std::string & solver_name)
 	{
-		EReturn ret_val = SUCCESS;
-		EReturn aux_rtn;
-
-		//!< Create the document
+        // /////////////////////////
+        // Create the document
 		xml_file.Clear();
 		if (xml_file.LoadFile(file_name.c_str()) != tinyxml2::XML_NO_ERROR)
 		{
-			INDICATE_FAILURE
-			;
+            INDICATE_FAILURE;
 			xml_file.Clear();
 			return PAR_ERR;
 		}
 		tinyxml2::XMLHandle root_handle(xml_file.RootElement());
 
-		//!< First resolve any includes that exist
+        // /////////////////////////
+        // First resolve any includes that exist
 		std::string file_path = file_name;
 		if (!ok(resolveParent(file_path)))
 		{
-			INDICATE_FAILURE
-			;
+            INDICATE_FAILURE;
 			xml_file.Clear();
-			return ret_val;
+            return FAILURE;
 		}
 		file_path += "/";
 		if (!ok(parseIncludes(root_handle, file_path)))
 		{
-			INDICATE_FAILURE
-			;
+            INDICATE_FAILURE;
 			xml_file.Clear();
-			return ret_val;
+            return FAILURE;
 		}
 
-		server = Server::Instance();
-		tinyxml2::XMLHandle server_handle(root_handle.FirstChildElement("Server"));
-		if (server_handle.ToElement())
-		{
-			if (!ok(server->initialise(server_handle)))
-			{
-				INDICATE_FAILURE
-				return FAILURE;
-			}
-		}
-		else
-		{
-			ERROR("EXOTica Server element is missing in the xml");
-			INDICATE_FAILURE
-			return FAILURE;
-		}
+        // /////////////////////////
+        // Initialise server
+        if(!ok(initialise(root_handle, server)))
+        {
+            INDICATE_FAILURE;
+            return FAILURE;
+        }
 
-		if (problem_name.empty())
-		{
-			ERROR("File '"<<file_name<<"' does not contain any known problem definitions.");
-			return FAILURE;
-		}
+        // /////////////////////////
+        // Initialise problem
+        if(!ok(initialise(root_handle, problem, problem_name, server)))
+        {
+            INDICATE_FAILURE;
+            return FAILURE;
+        }
 
-		tinyxml2::XMLHandle problem_handle(root_handle.FirstChildElement());
-		ret_val = FAILURE;
-		while (problem_handle.ToElement())
-		{
-			const char* atr = problem_handle.ToElement()->Attribute("name");
-			if (atr)
-			{
-				if (std::string(atr).compare(problem_name) == 0)
-				{
-					if (ok(PlanningProblem_fac::Instance().createObject(problem, problem_handle, server)))
-					{
-						ret_val = SUCCESS;
-						break;
-					}
-					else
-					{
-						INDICATE_FAILURE
-						;
-						return FAILURE;
-					}
-				}
-				else
-				{
-					problem_handle = problem_handle.NextSibling();
-					continue;
-				}
-			}
-			else
-			{
-				problem_handle = problem_handle.NextSibling();
-				ERROR("Element name was not specified!");
-				continue;
-			}
-		}
-		if (!ok(ret_val))
-		{
-			ERROR("File '"<<file_name<<"' does not contain the '"<<problem_name<<"' problem definition.");
-			return FAILURE;
-		}
+        // /////////////////////////
+        // Initialise solver
+        if(!ok(initialise(root_handle, solver, solver_name, server)))
+        {
+            INDICATE_FAILURE;
+            return FAILURE;
+        }
 
-		tinyxml2::XMLHandle solver_handle(root_handle.FirstChildElement());
-		ret_val = FAILURE;
-		while (solver_handle.ToElement())
-		{
-			if (!solver_name.empty())
-			{
-				const char* atr = solver_handle.ToElement()->Attribute("name");
-//				INFO(atr);
-				if (atr)
-				{
-					if (std::string(atr).compare(solver_name) != 0)
-					{
-						solver_handle = solver_handle.NextSibling();
-						ret_val == WARNING;
-						continue;
-					}
-				}
-				else
-				{
-					solver_handle = solver_handle.NextSibling();
-					ret_val == WARNING;
-					ERROR("Element name was not specified!");
-					continue;
-				}
-			}
-			aux_rtn = MotionSolver_fac::Instance().createObject(solver, solver_handle, server);
-			if (aux_rtn == WARNING)
-			{
-				solver_handle = solver_handle.NextSibling();
-				ret_val = WARNING;
-				continue;
-			}
-			else if (aux_rtn == SUCCESS)
-			{
-				return SUCCESS;
-			}
-			else
-			{
-				INDICATE_FAILURE
-				;
-				return aux_rtn;
-			}
-
-		}
-		if (!ok(ret_val))
-		{
-			if (!solver_name.empty())
-			{
-				ERROR("File '"<<file_name<<"' does not contain the '"<<solver_name<<"' solver.");
-			}
-			else
-			{
-				ERROR("File '"<<file_name<<"' does not contain any known solvers.");
-			}
-			INDICATE_FAILURE
-			return FAILURE;
-		}
-
-		return ret_val;
+        return SUCCESS;
 	}
+
+    EReturn Initialiser::initialise(const std::string & file_name, Server_ptr & server, std::vector<MotionSolver_ptr> & solver, std::vector<PlanningProblem_ptr> & problem,std::vector<std::string> & problem_name, std::vector<std::string> & solver_name)
+    {
+        // /////////////////////////
+        // Create the document
+        xml_file.Clear();
+        if (xml_file.LoadFile(file_name.c_str()) != tinyxml2::XML_NO_ERROR)
+        {
+            INDICATE_FAILURE;
+            xml_file.Clear();
+            return PAR_ERR;
+        }
+        tinyxml2::XMLHandle root_handle(xml_file.RootElement());
+
+        // /////////////////////////
+        // First resolve any includes that exist
+        std::string file_path = file_name;
+        if (!ok(resolveParent(file_path)))
+        {
+            INDICATE_FAILURE;
+            xml_file.Clear();
+            return FAILURE;
+        }
+        file_path += "/";
+        if (!ok(parseIncludes(root_handle, file_path)))
+        {
+            INDICATE_FAILURE;
+            xml_file.Clear();
+            return FAILURE;
+        }
+
+        // /////////////////////////
+        // Initialise server
+        if(!ok(initialise(root_handle, server)))
+        {
+            INDICATE_FAILURE;
+            return FAILURE;
+        }
+
+        // /////////////////////////
+        // Initialise problem
+        problem.resize(problem_name.size());
+        for(int i=0;i<problem_name.size();i++)
+        {
+            if(!ok(initialise(root_handle, problem[i], problem_name[i], server)))
+            {
+                INDICATE_FAILURE;
+                return FAILURE;
+            }
+        }
+
+        // /////////////////////////
+        // Initialise solver
+        solver.resize(solver_name.size());
+        for(int i=0;i<solver_name.size();i++)
+        {
+            if(!ok(initialise(root_handle, solver[i], solver_name[i], server)))
+            {
+                INDICATE_FAILURE;
+                return FAILURE;
+            }
+        }
+
+        return SUCCESS;
+    }
 }

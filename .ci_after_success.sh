e54dabcbff8a53fbd28b73ec3f624c5f22e2e2d4
Wolfgang Merkt <w.merkt@gmail.com>
2016-01-21 23:00:58 +0000

    Update after success script (clean up)

--- .ci_after_success.sh
+++ .ci_after_success.sh
@@ -3,13 +3,10 @@
 #
 # author: Wolfgang Merkt
 
-# Only build a deb for the master branch
-#if [ $TRAVIS_BRANCH -eq "master" ]
-#then
-  VERSION=`date +%Y.%m.%d-%H-%M`
+VERSION=`date +%Y.%m.%d-%H-%M`
 
-  mkdir -p ~/create_deb/DEBIAN
-  echo '
+mkdir -p ~/create_deb/DEBIAN
+echo '
 Package: exotica
 Version: '"$VERSION"'
 Architecture: all
@@ -20,12 +17,11 @@ Description: This is a preliminary debian package for EXOTica.
  Please note that this file is for build-server purposes only where dependencies are manually satisified. It will likely be broken on other systems.
 ' > ~/create_deb/DEBIAN/control
 
-  mkdir -p ~/create_deb/opt/ros/indigo/include
-  mkdir -p ~/create_deb/opt/ros/indigo/lib
-  mkdir -p ~/create_deb/opt/ros/indigo/share
-  cp -R ~/catkin_ws/install/include/ ~/create_deb/opt/ros/indigo/
-  cp -R ~/catkin_ws/install/lib/ ~/create_deb/opt/ros/indigo/
-  cp -R ~/catkin_ws/install/share/ ~/create_deb/opt/ros/indigo/
+mkdir -p ~/create_deb/opt/ros/indigo/include
+mkdir -p ~/create_deb/opt/ros/indigo/lib
+mkdir -p ~/create_deb/opt/ros/indigo/share
+cp -R ~/catkin_ws/install/include/ ~/create_deb/opt/ros/indigo/
+cp -R ~/catkin_ws/install/lib/ ~/create_deb/opt/ros/indigo/
+cp -R ~/catkin_ws/install/share/ ~/create_deb/opt/ros/indigo/
 
-  dpkg-deb --build ~/create_deb $TRAVIS_BUILD_DIR/exotica.deb
-#fi
+dpkg-deb --build ~/create_deb $TRAVIS_BUILD_DIR/exotica.deb

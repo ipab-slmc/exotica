482bfcebe8a8fb90fcc7c0a830653f152d31720a
Vladimir Ivan <v.ivan@ed.ac.uk>
2016-12-08 18:48:26 +0000

    Removed manual initialization from test

--- exotica/tests/test_initializers.cpp
+++ exotica/tests/test_initializers.cpp
@@ -1,8 +1,4 @@
 #include <exotica/Exotica.h>
-#include <ik_solver/IKsolverInitializer.h>
-#include <task_map/EffPositionInitializer.h>
-#include <exotica/TaskSqrErrorInitializer.h>
-#include <exotica/IKProblemInitializer.h>
 
 using namespace exotica;
 
@@ -15,21 +11,6 @@ bool testCore()
     if(Setup::getProblems().size()==0) {HIGHLIGHT_NAMED("EXOTica","Failed to find any problems."); return false;}
     if(Setup::getMaps().size()==0) {HIGHLIGHT_NAMED("EXOTica","Failed to find any maps."); return false;}
     if(Setup::getTasks().size()==0) {HIGHLIGHT_NAMED("EXOTica","Failed to find any Tasks."); return false;}
-    return true;
-}
-
-bool testManualInit()
-{
-    KinematicaInitializer kinematica(LimbInitializer("base"),{"joint1","joint2","joint3"});
-    SceneInitializer scene("MyScene",kinematica);
-    EffPositionInitializer map("Position","MyScene",false,{LimbInitializer("endeff"),LimbInitializer("endeff",Eigen::VectorTransform(0,0,0.5))});
-    TaskSqrErrorInitializer task("MinimizeError","Position",1e2);
-    Eigen::VectorXd W(3); W << 3,2,1;
-    IKProblemInitializer problem("MyProblem",scene,{map},{task},1e-5,W);
-    IKsolverInitializer solver("MySolver",false,1);
-    Server::Instance()->getModel("robot_description",urdf_string,srdf_string);
-    PlanningProblem_ptr any_problem = Setup::createProblem(problem);
-    MotionSolver_ptr any_solver = Setup::createSolver(solver);
     return true;
 }
 
@@ -85,7 +66,6 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "EXOTica_test_initializers");
     if(!testCore()) exit(2); HIGHLIGHT_NAMED("EXOTica","Core test passed.");
-    if(!testManualInit()) exit(2); HIGHLIGHT_NAMED("EXOTica","Manual initialization test passed.");
     if(!testGenericInit()) exit(2); HIGHLIGHT_NAMED("EXOTica","Generic initialization test passed.");
     if(!testXMLInit()) exit(2); HIGHLIGHT_NAMED("EXOTica","XML initialization test passed.");
     Setup::Destroy();

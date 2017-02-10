32d5195ba28b09806ad9a76eb83e92e459e09a24
Vladimir Ivan <v.ivan@ed.ac.uk>
2017-02-10 18:38:16 +0000

    Enabled visual debug in matlab demo

--- exotica_swig/matlab/examples/example_scene.m
+++ exotica_swig/matlab/examples/example_scene.m
@@ -3,7 +3,7 @@ tic;
 Core=exotica.Core;
 
 % Prepare an initializer
-SceneInit=exotica.Initializer('Scene',struct('Name','MyScene','Solver',{{'Kinematica',struct('Joints','lwr_arm_0_joint,lwr_arm_1_joint,lwr_arm_2_joint,lwr_arm_3_joint,lwr_arm_4_joint,lwr_arm_5_joint,lwr_arm_6_joint','Root',{{'Limb',struct('Segment','base')}})}}));
+SceneInit=exotica.Initializer('Scene',struct('Name','MyScene','VisualDebug','1','Solver',{{'Kinematica',struct('Joints','lwr_arm_0_joint,lwr_arm_1_joint,lwr_arm_2_joint,lwr_arm_3_joint,lwr_arm_4_joint,lwr_arm_5_joint,lwr_arm_6_joint','Root',{{'Limb',struct('Segment','base')}})}}));
 
 % Create a scene
 Scene=exotica.Scene('MyScene');

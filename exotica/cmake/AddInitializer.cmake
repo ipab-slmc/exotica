380bc7118eae1e4df449a14e8bbcdf4d290324dd
Wolfgang Merkt <w.merkt@gmail.com>
2017-06-01 01:23:21 +0100

    Proper fix to resolve #57 Note: initializers need to be added and generated BEFORE calling catkin_package

--- exotica/cmake/AddInitializer.cmake
+++ exotica/cmake/AddInitializer.cmake
@@ -12,6 +12,9 @@ foreach(path ${CMAKE_PREFIX_PATH})
 endforeach()
 
 macro(AddInitializer)
+  catkin_destinations()
+  file(MAKE_DIRECTORY ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_INCLUDE_DESTINATION})
+
   foreach(arg ${ARGN})
     list(APPEND _InitializerInputFiles ${CMAKE_CURRENT_SOURCE_DIR}/init/${arg}.in)
     list(APPEND _InitializerOutputFiles ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_INCLUDE_DESTINATION}/${arg}Initializer.h)
@@ -33,4 +36,17 @@ macro(GenInitializers)
     DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION})
   install(FILES ${_InitializerInputFiles}
     DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}/init)
+
+  GenInitializers_append_include_dirs()
 endmacro(GenInitializers)
+
+# akin to https://github.com/ros/gencpp/blob/indigo-devel/cmake/gencpp-extras.cmake.em#L54
+macro(GenInitializers_append_include_dirs)
+  if(NOT GenInitializers_APPENDED_INCLUDE_DIRS)
+    # make sure we can find generated messages and that they overlay all other includes
+    include_directories(BEFORE ${CATKIN_DEVEL_PREFIX}/include)
+    # pass the include directory to catkin_package()
+    list(APPEND ${PROJECT_NAME}_INCLUDE_DIRS ${CATKIN_DEVEL_PREFIX}/include)
+    set(GenInitializers_APPENDED_INCLUDE_DIRS TRUE)
+  endif()
+endmacro()

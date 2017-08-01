15834c72b744001eb12883ac1c3fb7b9ead1ead2
Vladimir Ivan <v.ivan@ed.ac.uk>
2017-08-01 18:23:03 +0100

    Add optional python version variable

--- pybind11/cmake/pybind.cmake
+++ pybind11/cmake/pybind.cmake
@@ -5,6 +5,15 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../pybind11/tools")
 
+message(WARN "Checking python executable: ${PYBIND_PYTHON_EXECUTABLE}")
+set(TEMP_PYTHON_EXECUTABLE ${PYTHON_EXECUTABLE})
+if(DEFINED PYBIND_PYTHON_EXECUTABLE)
+    message(WARN "Changing executable")
+    set(PYTHON_EXECUTABLE ${PYBIND_PYTHON_EXECUTABLE} CACHE INTERNAL "")
+endif()
+
+message(WARN "Using python executable: ${PYTHON_EXECUTABLE}")
+
 include(pybind11Tools)
 
 # Cache variables so pybind11_add_module can be used in parent projects
@@ -14,9 +23,16 @@ set(PYTHON_LIBRARIES ${PYTHON_LIBRARIES} CACHE INTERNAL "")
 set(PYTHON_MODULE_PREFIX ${PYTHON_MODULE_PREFIX} CACHE INTERNAL "")
 set(PYTHON_MODULE_EXTENSION ${PYTHON_MODULE_EXTENSION} CACHE INTERNAL "")
 
+message(WARN "Python: ${PYTHON_INCLUDE_DIRS}")
+message(WARN "Python: ${PYTHON_LIBRARIES}")
+message(WARN "Python: ${PYTHON_MODULE_PREFIX}")
+message(WARN "Python: ${PYTHON_MODULE_EXTENSION}")
+
+set(PYTHON_EXECUTABLE ${TEMP_PYTHON_EXECUTABLE} CACHE INTERNAL "")
+
 macro(pybind_add_module target_name other)
     pybind11_add_module(${ARGV})
-    target_link_libraries(${target_name} PRIVATE ${catkin_LIBRARIES})
+    target_link_libraries(${target_name} PRIVATE ${PYTHON_LIBRARIES} ${catkin_LIBRARIES})
     set_target_properties(${target_name} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${CATKIN_GLOBAL_PYTHON_DESTINATION})
     set(PYTHON_LIB_DIR ${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_PYTHON_DESTINATION})
     add_custom_command(TARGET ${target_name}

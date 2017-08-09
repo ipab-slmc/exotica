ab733bfe6b391f3a1d5005d4ab05ea31086a6b4a
Wolfgang Merkt <w.merkt@gmail.com>
2017-08-09 23:26:25 +0100

    Use packaged Eigen 3.2.10

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -xe
-wget https://bitbucket.org/eigen/eigen/get/3.2.6.zip
-unzip 3.2.6.zip
-cd eigen-eigen-c58038c56923
-mkdir build
-cd build
-cmake ..
-sudo make install
-cd ../..
+wget http://terminator.robots.inf.ed.ac.uk/apt/libeigen3-dev.deb
+sudo dpkg -i libeigen3-dev.deb
+rm libeigen3-dev.deb
+# wget https://bitbucket.org/eigen/eigen/get/3.2.6.zip
+# unzip 3.2.6.zip
+# cd eigen-eigen-c58038c56923
+# mkdir build
+# cd build
+# cmake ..
+# sudo make install
+# cd ../..

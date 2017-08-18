a0ed3554faa2201bd557e1c3d9dfcb6459d9906a
Vladimir Ivan <v.ivan@ed.ac.uk>
2017-08-18 10:49:32 +0100

    Added setModel to pyexotica

--- exotica_python/scripts/example_ompl_noros.py
+++ exotica_python/scripts/example_ompl_noros.py
@@ -7,8 +7,7 @@ from pyexotica.publish_trajectory import *
 
 (sol, prob)=exo.Initializers.loadXMLFull(exo.Setup.getPackagePath('exotica_examples')+'/resources/ompl_solver_demo.xml')
 # Set UDRF and SRDF file paths (overrides using robot_description)
-prob[1]['PlanningScene'][0][1]['URDF'] = exo.Setup.getPackagePath('exotica_examples')+'/resources/lwr_simplified.urdf'
-prob[1]['PlanningScene'][0][1]['SRDF'] = exo.Setup.getPackagePath('exotica_examples')+'/resources/lwr_simplified.srdf'
+exo.setModel(prob, exo.Setup.getPackagePath('exotica_examples')+'/resources/lwr_simplified.urdf', exo.Setup.getPackagePath('exotica_examples')+'/resources/lwr_simplified.srdf')
 
 problem = exo.Setup.createProblem(prob)
 solver = exo.Setup.createSolver(sol)

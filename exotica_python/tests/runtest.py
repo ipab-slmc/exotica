0eb780c2eada69ffdd56451c01f8578aaca0610e
Vladimir Ivan <v.ivan@ed.ac.uk>
2017-08-18 18:58:36 +0100

    Added python test

---
+++ exotica_python/tests/runtest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# This is a workaround for linurdf.so throwing an exception and killing the process on exit in ROS Indigo.
+
+import subprocess
+import os
+import sys
+
+tests=['core.py']
+
+for test in tests:
+    process=subprocess.Popen(['rosrun', 'exotica_python', test],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = process.stdout.readlines()
+    print(''.join(output))
+    if output[-1][0:11]!='>>SUCCESS<<':
+        print('Test '+test+' failed\n'+process.stderr.read())
+        os._exit(1)

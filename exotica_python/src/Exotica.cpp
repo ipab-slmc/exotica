27076bac66ff2dfa828f24948c646f246cddf0cb
Wolfgang Merkt <w.merkt@gmail.com>
2017-11-01 18:58:45 +0000

    exotica_python: Expose N and NumTasks for SamplingProblem

--- exotica_python/src/Exotica.cpp
+++ exotica_python/src/Exotica.cpp
@@ -575,6 +575,8 @@ PYBIND11_MODULE(_pyexotica, module)
     samplingProblem.def("setGoalState", &SamplingProblem::setGoalState);
     samplingProblem.def("getSpaceDim", &SamplingProblem::getSpaceDim);
     samplingProblem.def("getBounds", &SamplingProblem::getBounds);
+    samplingProblem.def_readonly("N", &SamplingProblem::N);
+    samplingProblem.def_readonly("NumTasks", &SamplingProblem::NumTasks);
     samplingProblem.def_readonly("Rho", &SamplingProblem::Rho);
     samplingProblem.def_readonly("y", &SamplingProblem::y);
     samplingProblem.def_readonly("Phi", &SamplingProblem::Phi);

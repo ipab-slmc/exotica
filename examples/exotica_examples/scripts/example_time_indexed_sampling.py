e38a95e102da67109abccd5714ec204c4ed4f797
yiming yang <yiming.yang@ed.ac.uk>
2017-11-07 17:54:56 +0000

    add new example for time indexed sampling solvers

---
+++ examples/exotica_examples/scripts/example_time_indexed_sampling.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pyexotica as exo
+from numpy import array
+from numpy import matrix
+from pyexotica.publish_trajectory import *
+
+exo.Setup.initRos()
+(sol, prob)=exo.Initializers.loadXMLFull('{exotica_examples}/resources/configs/time_indexed_sampling_demo.xml')
+problem = exo.Setup.createProblem(prob)
+solver = exo.Setup.createSolver(sol)
+solver.specifyProblem(problem)
+
+solution = solver.solve()
+
+publishTrajectory(solution, 3.0, problem)

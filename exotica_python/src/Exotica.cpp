f8386daefd1b2cde07dbf699ea7130011f3e5d5a
Wolfgang Merkt <w.merkt@gmail.com>
2017-11-24 14:23:47 +0000

    PlanningProblem: Add costEvolution to store cost evolution over iterations

--- exotica_python/src/Exotica.cpp
+++ exotica_python/src/Exotica.cpp
@@ -543,6 +543,7 @@ PYBIND11_MODULE(_pyexotica, module)
     planningProblem.def_property("startTime", &PlanningProblem::getStartTime, &PlanningProblem::setStartTime);
     planningProblem.def("getNumberOfProblemUpdates", &PlanningProblem::getNumberOfProblemUpdates);
     planningProblem.def("resetNumberOfProblemUpdates", &PlanningProblem::resetNumberOfProblemUpdates);
+    planningProblem.def("getCostEvolution", &PlanningProblem::getCostEvolution);
 
     // Problem types
     py::module prob = module.def_submodule("Problems", "Problem types");

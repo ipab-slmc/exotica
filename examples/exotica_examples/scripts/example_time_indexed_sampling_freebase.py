e37bddf8289b423e6e6f293d2a674d8688c2fd93
Yiming Yang <yiming.yang@ed.ac.uk>
2018-01-22 19:54:13 +0000

    Added free-base time indexed rrtconnect example

---
+++ examples/exotica_examples/scripts/example_time_indexed_sampling_freebase.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pyexotica as exo
+from numpy import array
+from numpy import matrix
+from pyexotica.publish_trajectory import *
+
+exo.Setup.initRos()
+(sol, prob)=exo.Initializers.loadXMLFull('{exotica_examples}/resources/configs/time_indexed_sampling_demo_freebase.xml')
+problem = exo.Setup.createProblem(prob)
+solver = exo.Setup.createSolver(sol)
+solver.specifyProblem(problem)
+
+solution = solver.solve()
+
+Ts = solution[:,0]
+solution = solution[:,1:]
+#print(Ts)
+#print(solution)
+publishTimeIndexedTrajectory(solution, Ts, problem)

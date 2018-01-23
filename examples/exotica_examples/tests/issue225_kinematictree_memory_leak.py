a90cb2223978cd8ec76ffb875cfad40c19a5b6e2
Wolfgang Merkt <w.merkt@gmail.com>
2018-01-23 22:46:46 +0000

    Add test for issue #225

---
+++ examples/exotica_examples/tests/issue225_kinematictree_memory_leak.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import time
+import resource
+import numpy as np
+import pyexotica as exo
+
+print(">>>>>>>>>>>>> Issue #225 Memory Leak")
+
+ompl = exo.Setup.loadSolver(
+    '{exotica_examples}/resources/configs/example_distance.xml')
+sc = ompl.getProblem().getScene()
+
+s = time.time()
+ompl.getProblem().update(np.zeros(7,))
+e = time.time()
+memory_usage_before = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+print(">>> Before:", e-s, " - Memory Usage Before:", memory_usage_before)
+
+print(">>> Loading and cleaning scene 500 times")
+for _ in range(500):
+    ompl.getProblem().update(np.zeros(7,))
+    sc.cleanScene()
+    sc.loadSceneFile(
+        '{exotica_examples}/resources/scenes/example_manipulate.scene')
+
+memory_usage_intermediate = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+print(">>> Intermediate:", " - Memory Usage Intermediate:",
+      memory_usage_intermediate)
+
+assert (memory_usage_intermediate - memory_usage_before) == 0
+
+print(">>> updateSceneFrames 10000 times")
+for _ in range(10000):
+    sc.updateSceneFrames()
+    sc.updateCollisionObjects()
+
+s = time.time()
+ompl.getProblem().update(np.ones(7,))
+e = time.time()
+memory_usage_after = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+print(">>> After:", e-s, " - Memory Usage After:", memory_usage_after)
+
+assert (memory_usage_after - memory_usage_before) == 0
+print(">>> LEAK:", memory_usage_after-memory_usage_before)
+
+print('>>SUCCESS<<')

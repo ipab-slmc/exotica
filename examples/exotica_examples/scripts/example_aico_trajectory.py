fdd8a354425ee57ece96f4eb866bec08ff608139
Vladimir Ivan <v.ivan@ed.ac.uk>
2018-01-31 01:40:00 +0000

    Removed local changes

--- examples/exotica_examples/scripts/example_aico_trajectory.py
+++ examples/exotica_examples/scripts/example_aico_trajectory.py
@@ -12,12 +12,10 @@ for t in range(0,problem.T):
         problem.setRho('Frame',0.0,t)
     else:
         problem.setRho('Frame',1e5,t)
-tt=exo.Timer()
+
 solution = solver.solve()
-print(tt.getDuration())
-cost=problem.getCostEvolution()
-plot(cost)
-#plot(solution)
+
+plot(solution)
 
 publishTrajectory(solution, problem.T*problem.tau, problem)
 

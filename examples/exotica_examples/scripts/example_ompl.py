93de005910cfd6ea19c55d331c35d55d1721800d
Vladimir Ivan <v.ivan@ed.ac.uk>
2018-03-18 23:23:59 +0000

    Simplified ompl examples

--- examples/exotica_examples/scripts/example_ompl.py
+++ examples/exotica_examples/scripts/example_ompl.py
@@ -6,13 +6,10 @@ from numpy import matrix
 from pyexotica.publish_trajectory import *
 
 exo.Setup.initRos()
-(sol, prob)=exo.Initializers.loadXMLFull('{exotica_examples}/resources/configs/ompl_solver_demo.xml')
-problem = exo.Setup.createProblem(prob)
-solver = exo.Setup.createSolver(sol)
-solver.specifyProblem(problem)
+solver = exo.Setup.loadSolver('{exotica_examples}/resources/configs/ompl_solver_demo.xml')
 
 solution = solver.solve()
 
 plot(solution)
 
-publishTrajectory(solution, 3.0, problem)
+publishTrajectory(solution, 3.0, solver.getProblem())

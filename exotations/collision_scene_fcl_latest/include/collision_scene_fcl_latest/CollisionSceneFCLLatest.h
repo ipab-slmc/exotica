f7449655e46422f28949a1995f890162eacd25d5
Wolfgang Merkt <w.merkt@gmail.com>
2018-05-21 20:54:06 +0100

    CollisionSceneFCLLatest: Move getCollisionDistance(object, selfcollision, disabletransformupdate) to public

--- exotations/collision_scene_fcl_latest/include/collision_scene_fcl_latest/CollisionSceneFCLLatest.h
+++ exotations/collision_scene_fcl_latest/include/collision_scene_fcl_latest/CollisionSceneFCLLatest.h
@@ -94,6 +94,7 @@ public:
     virtual std::vector<CollisionProxy> getCollisionDistance(const std::string& o1, const std::string& o2);
     virtual std::vector<CollisionProxy> getCollisionDistance(const std::string& o1, const bool& self = true);
     virtual std::vector<CollisionProxy> getCollisionDistance(const std::vector<std::string>& objects, const bool& self = true);
+    virtual std::vector<CollisionProxy> getCollisionDistance(const std::string& o1, const bool& self = true, const bool& disableCollisionSceneUpdate = false);
 
     /**
        * @brief      Gets the collision world links.
@@ -126,8 +127,6 @@ private:
     std::shared_ptr<fcl::CollisionObjectd> constructFclCollisionObject(long i, std::shared_ptr<KinematicElement> element);
     static void checkCollision(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, CollisionData* data);
     static void computeDistance(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, DistanceData* data);
-
-    virtual std::vector<CollisionProxy> getCollisionDistance(const std::string& o1, const bool& self = true, const bool& disableCollisionSceneUpdate = false);
 
     std::map<std::string, std::shared_ptr<fcl::CollisionObjectd>> fcl_cache_;
     std::vector<fcl::CollisionObjectd*> fcl_objects_;

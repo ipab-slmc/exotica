9a0013e5adfcf7e2955bb67bf2697875d65272d9
Wolfgang Merkt <w.merkt@gmail.com>
2018-05-23 21:03:26 +0100

    ompl_solver_py: Expose state-space and algorithm properties to Python

--- exotations/solvers/ompl_solver/src/ompl_solver/ompl_py.cpp
+++ exotations/solvers/ompl_solver/src/ompl_solver/ompl_py.cpp
@@ -15,12 +15,24 @@ PYBIND11_MODULE(ompl_solver_py, module)
 
     py::module::import("pyexotica");
 
-    py::class_<RRT, std::shared_ptr<RRT>, MotionSolver> rrt(module, "RRT");
-    py::class_<RRTConnect, std::shared_ptr<RRTConnect>, MotionSolver> rrtcon(module, "RRTConnect");
-    py::class_<EST, std::shared_ptr<EST>, MotionSolver> est(module, "EST");
-    py::class_<KPIECE, std::shared_ptr<KPIECE>, MotionSolver> kpiece(module, "KPIECE");
-    py::class_<BKPIECE, std::shared_ptr<BKPIECE>, MotionSolver> bkpiece(module, "BKPIECE");
-    py::class_<PRM, std::shared_ptr<PRM>, MotionSolver> prm(module, "PRM");
+    py::class_<OMPLsolver<SamplingProblem>, std::shared_ptr<OMPLsolver<SamplingProblem>>, MotionSolver> omplSolver(module, "OMPLMotionSolver");
+    omplSolver.def("getRandomSeed", &OMPLsolver<SamplingProblem>::getRandomSeed);
+
+    // State-space properties exposed via OMPLsolver
+    omplSolver.def("getMaximumExtent", &OMPLsolver<SamplingProblem>::getMaximumExtent);
+    omplSolver.def("getLongestValidSegmentLength", &OMPLsolver<SamplingProblem>::getLongestValidSegmentLength);
+    omplSolver.def("setLongestValidSegmentFraction", &OMPLsolver<SamplingProblem>::setLongestValidSegmentFraction);
+    omplSolver.def("setValidSegmentCountFactor", &OMPLsolver<SamplingProblem>::setValidSegmentCountFactor);
+    omplSolver.def("getValidSegmentCountFactor", &OMPLsolver<SamplingProblem>::getValidSegmentCountFactor);
+
+    py::class_<RRT, std::shared_ptr<RRT>, OMPLsolver<SamplingProblem>> rrt(module, "RRT");
+    py::class_<RRTConnect, std::shared_ptr<RRTConnect>, OMPLsolver<SamplingProblem>> rrtcon(module, "RRTConnect");
+    rrtcon.def("getRange", &RRTConnect::getRange);
+    rrtcon.def("setRange", &RRTConnect::setRange);
+    py::class_<EST, std::shared_ptr<EST>, OMPLsolver<SamplingProblem>> est(module, "EST");
+    py::class_<KPIECE, std::shared_ptr<KPIECE>, OMPLsolver<SamplingProblem>> kpiece(module, "KPIECE");
+    py::class_<BKPIECE, std::shared_ptr<BKPIECE>, OMPLsolver<SamplingProblem>> bkpiece(module, "BKPIECE");
+    py::class_<PRM, std::shared_ptr<PRM>, OMPLsolver<SamplingProblem>> prm(module, "PRM");
     prm.def("growRoadmap", &PRM::growRoadmap);
     prm.def("expandRoadmap", &PRM::expandRoadmap);
     prm.def("clear", &PRM::clear);
@@ -30,7 +42,7 @@ PYBIND11_MODULE(ompl_solver_py, module)
     prm.def("milestoneCount", &PRM::milestoneCount);
     prm.def_property("multiQuery", &PRM::isMultiQuery, &PRM::setMultiQuery);
 
-    py::class_<LazyPRM, std::shared_ptr<LazyPRM>, MotionSolver> lprm(module, "LazyPRM");
+    py::class_<LazyPRM, std::shared_ptr<LazyPRM>, OMPLsolver<SamplingProblem>> lprm(module, "LazyPRM");
     lprm.def("clear", &LazyPRM::clear);
     lprm.def("clearQuery", &LazyPRM::clearQuery);
     lprm.def("setup", &LazyPRM::setup);

a70af53bc19b438434e2bf9696038d408e3e3020
Wolfgang Merkt <w.merkt@gmail.com>
2018-05-28 14:04:16 +0100

    CollisionScene add debug information

--- exotations/collision_scene_fcl/src/CollisionSceneFCL.cpp
+++ exotations/collision_scene_fcl/src/CollisionSceneFCL.cpp
@@ -47,6 +47,7 @@ namespace exotica
 {
 CollisionSceneFCL::CollisionSceneFCL()
 {
+    HIGHLIGHT_NAMED("CollisionSceneFCL", "FCL version: " << FCL_VERSION);
 }
 
 CollisionSceneFCL::~CollisionSceneFCL()

90734a23c30fb2704d553612625460948f287d14
Henrique Ferrolho <henriqueferrolho@gmail.com>
2018-06-25 15:05:39 +0100

    Indentation and formatting adjustments

--- examples/exotica_examples/scripts/example_ik_valkyrie.py
+++ examples/exotica_examples/scripts/example_ik_valkyrie.py
@@ -1,46 +1,47 @@
 #!/usr/bin/env python
 
-import pyexotica as exo
-from numpy import array
-from numpy import matrix
 import math
-from pyexotica.publish_trajectory import *
 from time import sleep
+
+import pyexotica as exo
+from numpy import array, matrix
+from pyexotica.publish_trajectory import *
+
 
 def com(t):
     return array([-0.1, math.sin(t * 0.5 * math.pi) * 0.3, 0.9])
 
+
 exo.Setup.initRos()
-(sol, prob)=exo.Initializers.loadXMLFull('{exotica_examples}/resources/configs/ik_quasistatic_valkyrie.xml')
+(sol, prob) = exo.Initializers.loadXMLFull('{exotica_examples}/resources/configs/ik_quasistatic_valkyrie.xml')
 problem = exo.Setup.createProblem(prob)
 solver = exo.Setup.createSolver(sol)
 solver.specifyProblem(problem)
 
 tick = exo.Timer()
-t=0.0;
-q=problem.startState
+t = 0.0
+q = problem.startState
 print('Publishing IK')
 signal.signal(signal.SIGINT, sigIntHandler)
-pose=[0]*20;
-stability=None
+pose = [0] * 20
+stability = None
 for task in problem.getTasks():
-    if task.getName()=='Stability':
-        stability=task
+    if task.getName() == 'Stability':
+        stability = task
         break
 if not stability:
     quit(2)
 while True:
     try:
-        problem.setGoal('CoM',com(tick.getDuration()))
-        pose[3]=math.sin(tick.getDuration() * 0.25 * math.pi)*0.8;
-        pose[13]=-math.sin(tick.getDuration() * 0.25 * math.pi)*0.8;
-        problem.setGoal('Pose',pose)
+        problem.setGoal('CoM', com(tick.getDuration()))
+        pose[3] = math.sin(tick.getDuration() * 0.25 * math.pi) * 0.8
+        pose[13] = -math.sin(tick.getDuration() * 0.25 * math.pi) * 0.8
+        problem.setGoal('Pose', pose)
         problem.startState = q
-        stability.debugMode=False
+        stability.debugMode = False
         q = solver.solve()[0]
-        stability.debugMode=True
+        stability.debugMode = True
         problem.update(q)
         publishPose(q, problem)
     except KeyboardInterrupt:
         break
-

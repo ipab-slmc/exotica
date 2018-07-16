b95b8fbaac7241cb104a468a8a9f6f683fa42778
Wolfgang Merkt <w.merkt@gmail.com>
2018-07-16 17:53:43 +0100

    Printable: Eigen::Affine3d/Eigen::Isometry3d toString

--- exotica/src/Tools/Printable.cpp
+++ exotica/src/Tools/Printable.cpp
@@ -13,3 +13,15 @@ std::string toString(const KDL::Frame& s)
     s.M.GetQuaternion(x, y, z, w);
     return "([" + std::to_string(s.p.data[0]) + " " + std::to_string(s.p.data[1]) + " " + std::to_string(s.p.data[2]) + "] [" + std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + " " + std::to_string(w) + "])";
 }
+
+std::string toString(const Eigen::Isometry3d& s)
+{
+    Eigen::Quaterniond quat(s.rotation());
+    return "([" + std::to_string(s.translation().x()) + " " + std::to_string(s.translation().y()) + " " + std::to_string(s.translation().z()) + "] [" + std::to_string(quat.x()) + " " + std::to_string(quat.y()) + " " + std::to_string(quat.z()) + " " + std::to_string(quat.w()) + "])";
+}
+
+std::string toString(const Eigen::Affine3d& s)
+{
+    Eigen::Quaterniond quat(s.rotation());
+    return "([" + std::to_string(s.translation().x()) + " " + std::to_string(s.translation().y()) + " " + std::to_string(s.translation().z()) + "] [" + std::to_string(quat.x()) + " " + std::to_string(quat.y()) + " " + std::to_string(quat.z()) + " " + std::to_string(quat.w()) + "])";
+}

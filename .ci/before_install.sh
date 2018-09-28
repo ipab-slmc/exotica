1d27c1195eb6c652d5a6838c92360886ad4a2060
Christian Rauch <Christian.Rauch@ed.ac.uk>
2018-09-28 16:37:19 +0100

    update CI to Ubuntu xenial and ROS kinetic

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xe
-wget http://terminator.robots.inf.ed.ac.uk/apt/libeigen3-dev.deb
-sudo dpkg -i libeigen3-dev.deb
-rm libeigen3-dev.deb
+# wget http://terminator.robots.inf.ed.ac.uk/apt/libeigen3-dev.deb
+# sudo dpkg -i libeigen3-dev.deb
+# rm libeigen3-dev.deb
 # wget https://bitbucket.org/eigen/eigen/get/3.2.6.zip
 # unzip 3.2.6.zip
 # cd eigen-eigen-c58038c56923

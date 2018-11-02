965dcadfbad0c6f84390a3d0b3091fb21f567a6b
cmower <chris.mower@ed.ac.uk>
2018-11-02 15:05:13 +0000

    Adding three tasks maps that account for velocity, acceleration, and jerk motion smoothing using backward finite differencing.
    Details:
    
    * Tasks maps Phi_v(q) = qdot, Phi_a(q) = qddot, Phi_j(q) = qdddot where each is estimated with backwards finite differencing using the previous solutions.
    * The previous solutions must be updated from the higher level script, e.g. see teleop example script example_ik_with_jnt_smoothing.py.
    
    State:
    
    * Compiles/runs as expected.
    
    testing newly exposed getTaskMaps
    
    Cleaning code and adding teleop example
    
    Exposing getTaskMaps to Python.
    
    Adding Rho for each task.
    
    Publish joint state and eff state.
    
    Damp/limit/modify motion.
    
    * x,y,z limits
    * damping controller input
    * user modifiable rho facility
    
    updated for pull request review
    
    moving constructor/destructor implementation to header files + code formatting
    
    [task_map] Apply formatting after rebase
    
    Updates to joint smoothing task maps [still not ready to merge, failing on execution]
    * name changes JointVelBackFiniteDiff -> JointVelocityBackwardDifference (and similar)
    * minor code updates
    * rm initPrevJointState
    * get start state from xml (preferably from <StartState> but this requires PlanningScene to be set, perhaps exotica requires deeper changes to handle this)
    
    [task maps] Updates for review
    * updated documentation in exo plugins
    * code formatting
    * moving initialization to follow the scene_ variable assignment
    * adding comments in code (using doxygen)
    * user required to specify StartState
    * cleaning code
    * fix input checking in update methods
    * var name change e_ -> backward_difference_params_
    * backward_difference_params_ now const
    * note I_ cannot be made constant since we can't declare it pre-compilation (it depends on N_ that is declared at run-time)
    
    [exotica_examples] updates for review
    * code cleaning
    * adding start state to each task map
    * rm nominal state, not needed for this example
    * extract task maps interfaces at __init__ rather than at every update() loop
    * rm joint state and eff state publishers (not required)
    
    [task_map] StartState Required parameter -> Optional parameter
    * StartState defaults to zeros if not specified
    
    [exotica_examples] example of optional StartState parameter
    
    [task maps] updates for review
    * moved documentation to header files
    * rm constexpr from namespace
      * `backward_difference_params_` are now reverted to Vector3d
      * Suggestions in review to make this attribute a constexpr fail to compile.
    * Initialize `q_` fix
    * Typo fixes
    * Code formatting
    
    [exotica_examples] updates for review
    * Python3 compatibility fix
    * joy topic now remapable
    
    [exotica_examples] variable name change: `rho` -> `Rho_Smoothing`

---
+++ examples/exotica_examples/scripts/example_ik_with_jnt_smoothing.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import rospy
+import pyexotica as exo
+import numpy as np
+import signal
+from pyexotica.publish_trajectory import publishPose, sigIntHandler
+from time import sleep
+import task_map_py
+from sensor_msgs.msg import Joy, JointState
+from std_msgs.msg import Float64MultiArray as FloatArray
+
+DT = 1.0/100.0 # 100 HZ
+DAMP = 0.005
+
+SMOOTHING_TASK_MAPS = ['JointVel', 'JointAcc', 'JointJerk']
+
+XLIM = [0.0, 0.75]
+YLIM = [-0.5, 0.5]
+ZLIM = [0, 1.0]
+
+LIMITS = np.array([XLIM,\
+                   YLIM,\
+                   ZLIM])
+
+class Example(object):
+
+    def __init__(self):
+
+        # Set init variables
+        self.q=np.array([0.0]*7)
+        self.joy = None
+        self.eff = np.array([0.6, -0.1, 0.5, 0, 0, 0])
+
+        # Setup exotica
+        self.solver = exo.Setup.loadSolver('{exotica_examples}/resources/configs/ik_with_jnt_smoothing.xml')
+        self.problem = self.solver.getProblem()
+        self.task_maps = self.problem.getTaskMaps()
+        
+        # Setup ros subscriber
+        self.sub={}
+        self.sub['joy'] = rospy.Subscriber('joy', Joy, self.callback)
+        
+    def callback(self, msg):
+        self.joy = msg
+
+    def update(self, event):
+        if self.joy is None: return
+
+        # Compute eff goal
+        dx = self.joy.axes[4]
+        dy = self.joy.axes[0]
+        dz = self.joy.axes[1]
+        eff = self.eff + DAMP * np.array([dx, dy, dz, 0, 0, 0])
+
+        # Check limits
+        for i in xrange(3):
+            if not LIMITS[i,0] <= eff[i] <= LIMITS[i,1]:
+                eff[i] = self.eff[i]
+
+        # Set eff goal 
+        self.problem.setGoal('Position', eff)
+        #self.problem.getScene().attachObjectLocal('Goal', '', exo.KDLFrame(eff))
+                
+        # Setup problem
+        if self.joy.buttons[0]:
+            # Button [0] pressed -> turn smoothing off -> report 
+            Rho_Smoothing = 0
+            print("Smoothing OFF")
+        else:
+            # Button [0] released -> turn smoothing on -> report
+            Rho_Smoothing = 1
+            print("Smoothing ON")
+
+        # Set start state, rho, and previous joint states 
+        self.problem.startState = self.q
+        for name in SMOOTHING_TASK_MAPS:
+            self.problem.setRho(name, Rho_Smoothing)
+            self.task_maps[name].setPrevJointState(self.q)
+
+        # Solve and publish
+        q = self.solver.solve()[0]
+        publishPose(q, self.problem)
+
+        # Set new as old
+        self.q = q
+        self.eff = eff
+
+if __name__=='__main__':
+    rospy.init_node('example_ik_with_jnt_smoothing_node')
+    exo.Setup.initRos()
+    signal.signal(signal.SIGINT, sigIntHandler)
+    rospy.Timer(rospy.Duration(DT), Example().update)
+    rospy.spin()
---
+++ exotations/task_maps/task_map/include/task_map/JointAccelerationBackwardDifference.h
@@ -0,0 +1,90 @@
+/*
+ *      Author: Christopher E. Mower
+ *
+ * Copyright (c) 2018, University of Edinburgh
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of  nor the names of its contributors may be used to
+ *    endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef JOINT_ACCELERATION_BACKWARD_DIFFERENCE_H_
+#define JOINT_ACCELERATION_BACKWARD_DIFFERENCE_H_
+
+#include <exotica/TaskMap.h>
+#include <task_map/JointAccelerationBackwardDifferenceInitializer.h>
+
+namespace exotica
+{
+/** \brief Time-derivative estimation by backward differencing.
+   *
+   *  JointAccelerationBackwardDifference uses backward differencing to
+   *  estimate the second time derivative of the joint state.
+   *  
+   *  For more information see:
+   *    http://mathworld.wolfram.com/BackwardDifference.html
+   * 
+   *  Here, x+qbd_ represents the simplified estimate of the second
+   *  time derivative.
+   * 
+   */
+
+class JointAccelerationBackwardDifference : public TaskMap, public Instantiable<JointAccelerationBackwardDifferenceInitializer>
+{
+public:
+    JointAccelerationBackwardDifference();
+    virtual ~JointAccelerationBackwardDifference();
+    virtual void assignScene(Scene_ptr scene);
+    virtual void Instantiate(JointAccelerationBackwardDifferenceInitializer& init);
+
+    /** \brief Logs previous joint state.
+     *
+     *  setPrevJointState must be called after solve is called in a Python/C++ script is called 
+     *  to ensure the time-derivatives are appropriately approximated. 
+     *  Each previous joint state is pushed back by collumn and the given joint_state is placed
+     *  in q_.col(0). 
+     *  Finally, we compute the new qbd_.
+     * 
+     */
+    void setPrevJointState(Eigen::VectorXdRefConst joint_state);
+
+    virtual void update(Eigen::VectorXdRefConst x, Eigen::VectorXdRef phi);
+    virtual void update(Eigen::VectorXdRefConst x, Eigen::VectorXdRef phi, Eigen::MatrixXdRef J);
+    virtual int taskSpaceDim();
+
+private:
+    JointAccelerationBackwardDifferenceInitializer init_;  //<! Task map initializer.
+    Scene_ptr scene_;                                      //<! Scene pointer.
+    int N_;                                                //!< Number of dofs for robot.
+    Eigen::Vector2d backward_difference_params_;           //<! Binomial cooeficient parameters.
+    Eigen::MatrixXd q_;                                    //!< Log of previous two joint states.
+    Eigen::VectorXd qbd_;                                  //!< x+qbd_ is a simplifed estiamte of the thrird time derivative.
+    Eigen::MatrixXd I_;                                    //!< Identity matrix.
+};
+
+typedef std::shared_ptr<JointAccelerationBackwardDifference> JointAccelerationBackwardDifference_ptr;  //!< Task Map smart pointer.
+}
+
+#endif /* JOINT_ACCELERATION_BACKWARD_DIFFERENCE_H_ */

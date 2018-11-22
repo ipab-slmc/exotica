13bc210d3bb926272882997579aeeecdb750d9fa
Wolfgang Merkt <w.merkt@gmail.com>
2018-11-22 15:17:39 +0000

    Documentation: Update deploy script

--- .ci/deploy_documentation.sh
+++ .ci/deploy_documentation.sh
@@ -4,17 +4,20 @@ set -e
 if [ `lsb_release -c -s` = "xenial" ]; then
     if [ "${TRAVIS_BRANCH}" = "master" ]; then
         GIT_COMMIT=`git rev-parse HEAD`
+        echo "Building documentation for commit $GIT_COMMIT"
         /bin/bash ./.ci/build_documentation.sh > /dev/null 2>&1
         git checkout --orphan gh-pages
         mkdir old ; mv * old/ ; mv old/exotica/doc/_build/html/* . ; rm -rf old ; touch .nojekyll ; rm -rf .ci ; rm .clang-format .travis.yml .gitmodules .gitignore .rosinstall*
         # Check if user configured email and name
         GIT_CONFIG_CHECK=`git config --list --global`
         if [[ $GIT_CONFIG_CHECK != *"user.email"* ]]; then
+            echo "Setting global git user settings"
             git config --global user.email "builds@travis-ci.com"
             git config --global user.name "Travis CI"
         fi
         git add --all
         git commit -m "Update public documentation for $GIT_COMMIT"
+        echo "Pushing public documentation to GitHub Pages"
         git push --quiet --force https://$GITHUBKEY@github.com/ipab-slmc/exotica.git gh-pages:gh-pages > /dev/null 2>&1
     else
         echo "Not master, not building documentation."

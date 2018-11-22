f09524031ff249395fc57ab9459e51310652d99e
Wolfgang Merkt <w.merkt@gmail.com>
2018-11-22 14:55:08 +0000

    Documentation: deploy for master branch on CI

--- .ci/deploy_documentation.sh
+++ .ci/deploy_documentation.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Build and deploy documentation
-# - cd $TRAVIS_BUILD_DIR
-# - git config --global user.email "builds@travis-ci.com"
-# - git config --global user.name "Travis CI"
-# - bash .ci/build_documentation.sh > /dev/null 2>&1
-# - git checkout --orphan gh-pages
-# - mkdir old ; mv * old/ ; mv old/exotica/doc/_build/html/* . ; rm -rf old ; touch .nojekyll ; rm -rf .ci ; rm .clang-format .travis.yml .gitmodules .gitignore
-# - git add --all
-# - git commit -m "Update public documentation for $GIT_COMMIT"
-# - git push --quiet --force https://$GITHUBKEY@github.com/ipab-slmc/exotica.git gh-pages:gh-pages > /dev/null 2>&1
-
+set -e
 if [ `lsb_release -c -s` = "xenial" ]; then
     if [ "${TRAVIS_BRANCH}" = "master" ]; then
-        /bin/bash b./uild_documentation.sh > /dev/null 2>&1
+        GIT_COMMIT = `git rev-parse HEAD`
+        /bin/bash ./build_documentation.sh > /dev/null 2>&1
         git checkout --orphan gh-pages
         mkdir old ; mv * old/ ; mv old/exotica/doc/_build/html/* . ; rm -rf old ; touch .nojekyll ; rm -rf .ci ; rm .clang-format .travis.yml .gitmodules .gitignore
+        # Check if user configured email and name
+        GIT_CONFIG_CHECK = `git config --list --global`
+        if [[ $GIT_CONFIG_CHECK != *"user.email"* ]]; then
+            git config --global user.email "builds@travis-ci.com"
+            git config --global user.name "Travis CI"
+        fi
         git add --all
+        git commit -m "Update public documentation for $GIT_COMMIT"
+        git push --quiet --force https://$GITHUBKEY@github.com/ipab-slmc/exotica.git gh-pages:gh-pages > /dev/null 2>&1
     else
         echo "Not master, not building documentation."
     fi

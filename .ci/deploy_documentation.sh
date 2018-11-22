d03ac548d048440ac5885073851bf431c8227cad
Wolfgang Merkt <w.merkt@gmail.com>
2018-11-22 15:11:46 +0000

    Documentation: Fix deploy script

--- .ci/deploy_documentation.sh
+++ .ci/deploy_documentation.sh
@@ -3,12 +3,12 @@
 set -e
 if [ `lsb_release -c -s` = "xenial" ]; then
     if [ "${TRAVIS_BRANCH}" = "master" ]; then
-        GIT_COMMIT = `git rev-parse HEAD`
+        GIT_COMMIT=`git rev-parse HEAD`
         /bin/bash ./.ci/build_documentation.sh > /dev/null 2>&1
         git checkout --orphan gh-pages
         mkdir old ; mv * old/ ; mv old/exotica/doc/_build/html/* . ; rm -rf old ; touch .nojekyll ; rm -rf .ci ; rm .clang-format .travis.yml .gitmodules .gitignore .rosinstall*
         # Check if user configured email and name
-        GIT_CONFIG_CHECK = `git config --list --global`
+        GIT_CONFIG_CHECK=`git config --list --global`
         if [[ $GIT_CONFIG_CHECK != *"user.email"* ]]; then
             git config --global user.email "builds@travis-ci.com"
             git config --global user.name "Travis CI"

e6a5f2ec8c1cb2b30ffd64805da772f819f3eea9
Wolfgang Merkt <w.merkt@gmail.com>
2018-11-25 17:22:24 +0000

    [exotica_examples] Update example_distance.py example

--- examples/exotica_examples/scripts/example_distance.py
+++ examples/exotica_examples/scripts/example_distance.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 
 import pyexotica as exo
-from pyexotica.publish_trajectory import *
+from pyexotica.publish_trajectory import sigIntHandler
+from time import sleep
+import signal
 import math
 
 exo.Setup.initRos()
-ompl=exo.Setup.loadSolver('{exotica_examples}/resources/configs/example_distance.xml')
-sc=ompl.getProblem().getScene()
-sc.loadSceneFile('{exotica_examples}/resources/scenes/example_distance.scene')
+ompl = exo.Setup.loadSolver('{exotica_examples}/resources/configs/example_distance.xml')
+sc = ompl.getProblem().getScene()
 
-dt=0.01
-t=0.0
+dt = 0.01
+t = 0.0
 signal.signal(signal.SIGINT, sigIntHandler)
 while True:
     try:
         sc.Update([math.sin(t/2.0)*0.5]*7)
-        p=sc.getCollisionDistance(False)
+        p = sc.getCollisionDistance(False)
         sc.getSolver().publishFrames()
         sc.publishProxies(p)
-        t=t+dt
+        t = t+dt
         sleep(dt)
     except KeyboardInterrupt:
         break

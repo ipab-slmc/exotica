43f7b9eb0c5cdb35dd266112873574e9b6ddb814
Vladimir Ivan <v.ivan@ed.ac.uk>
2018-11-28 13:01:55 +0000

    [exotica] Workaround for inaccurate KDL quatertion conversion

--- exotica/src/Tools/Conversions.cpp
+++ exotica/src/Tools/Conversions.cpp
@@ -122,9 +122,8 @@ Eigen::VectorXd setRotation(const KDL::Rotation& data, RotationType type)
     Eigen::VectorXd ret;
     switch (type)
     {
-        case RotationType::QUATERNION:
-            ret.resize(4);
-            data.GetQuaternion(ret(0), ret(1), ret(2), ret(3));
+        case RotationType::QUATERNION:            
+            ret = Eigen::Quaterniond(Eigen::Map<const Eigen::Matrix3d>(&data.data[0]).transpose()).coeffs();
             return ret;
         case RotationType::RPY:
             ret.resize(3);

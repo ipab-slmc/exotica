04fcca05c4feb6077647352f11c012f2b35facb2
Christian Rauch <Christian.Rauch@ed.ac.uk>
2018-12-14 16:27:34 +0000

    cleanup debug output

--- exotations/solvers/exotica_levenberg_marquardt_solver/src/LevenbergMarquardt.cpp
+++ exotations/solvers/exotica_levenberg_marquardt_solver/src/LevenbergMarquardt.cpp
@@ -69,22 +69,13 @@ void LevenbergMarquardt::Solve(Eigen::MatrixXd& solution) {
 
         yd = prob_->Cost.S * prob_->Cost.ydiff;
 
-        if(debug_) std::cout << "yd: " << std::endl << std::setprecision(3) << yd.transpose() << std::endl;
-
         // weighted sum of squares
         error_prev = error;
         error = prob_->getScalarCost();
 
-        if(debug_) std::cout << "err: " << std::endl << error << ", " << yd.cwiseAbs().sum() << std::endl;
-
         prob_->setCostEvolution(i, error);
 
-        if(debug_) std::cout << "mse: " << error/yd.size() << std::endl;
-
         J = prob_->Cost.S*prob_->Cost.J;
-
-//        std::cout << "S: " << std::endl << std::setprecision(3) << prob_->Cost.S << std::endl;
-        if(debug_) std::cout << "J: " << std::endl << std::setprecision(3) << J << std::endl;
 
         // source: https://uk.mathworks.com/help/optim/ug/least-squares-model-fitting-algorithms.html, eq. 13
 
@@ -99,7 +90,7 @@ void LevenbergMarquardt::Solve(Eigen::MatrixXd& solution) {
             }
         }
 
-        if(debug_) std::cout << "damping: " << lambda << std::endl;
+        if(debug_) HIGHLIGHT_NAMED("Levenberg-Marquardt", "damping: " << lambda);
 
         Eigen::MatrixXd M;
         if(parameters_.ScaleProblem=="none") {
@@ -112,21 +103,7 @@ void LevenbergMarquardt::Solve(Eigen::MatrixXd& solution) {
             throw std::runtime_error("no ScaleProblem of type "+parameters_.ScaleProblem);
         }
 
-        // condition number
-        // https://forum.kde.org/viewtopic.php?f=74&t=117430#p292018
-        if(debug_) {
-            const Eigen::MatrixXd A = J.transpose()*J + lambda*M;
-//            std::cout << "A:" << std::endl << A <<std::endl;
-            Eigen::JacobiSVD<Eigen::MatrixXd> svd(A);
-            double cond = svd.singularValues()(0) / svd.singularValues()(svd.singularValues().size()-1);
-            std::cout << "cond: " << cond << std::endl;
-        }
-
         qd = (J.transpose()*J + lambda*M).completeOrthogonalDecomposition().solve(J.transpose()*yd);
-
-        if(debug_) std::cout << "solution?: " << ((J.transpose()*J + lambda*M)*qd).isApprox(J.transpose()*yd) << std::endl;
-
-        if(debug_) std::cout << "qd: " << std::endl << std::setprecision(3) << qd.transpose() << std::endl;
 
         if (parameters_.Alpha.size() == 1) {
             q -= qd * parameters_.Alpha[0];
@@ -135,10 +112,8 @@ void LevenbergMarquardt::Solve(Eigen::MatrixXd& solution) {
             q -= qd.cwiseProduct(parameters_.Alpha);
         }
 
-        if(debug_) std::cout << "q: " << std::endl << std::setprecision(3) << q.transpose() << std::endl;
-
         if (qd.norm() < parameters_.Convergence) {
-            if (debug_) HIGHLIGHT_NAMED("LevenbergMarquardt", "Reached convergence (" << qd.norm() << " < " << parameters_.Convergence << ")");
+            if (debug_) HIGHLIGHT_NAMED("Levenberg-Marquardt", "Reached convergence (" << qd.norm() << " < " << parameters_.Convergence << ")");
             break;
         }
     }

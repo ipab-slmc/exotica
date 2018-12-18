84fc3fa3436ca2bd9e50e334f41b7fd3673d148d
Wolfgang Merkt <w.merkt@gmail.com>
2018-12-18 22:13:52 +0000

    [exotica_examples] Update test_ompl_solver_bounds for new API

--- examples/exotica_examples/test/test_ompl_solver_bounds.py
+++ examples/exotica_examples/test/test_ompl_solver_bounds.py
@@ -6,11 +6,11 @@ class OMPLLockedBoundsCase(unittest.TestCase):
 
     def test_lock_bounds(self):
         # self.assertTrue(True)
-        ompl=exo.Setup.loadSolver('{exotica_examples}/resources/configs/example_manipulate_ompl.xml')
+        ompl = exo.Setup.load_solver('{exotica_examples}/resources/configs/example_manipulate_ompl.xml')
 
         # set start and goal state
-        ompl.getProblem().startState = [1.5035205538438838, 0.8730168650583787, -1.6298590879018438, 1.7106630821349438, -0.8789956712153559, 0.1278222471656531, 0.0]
-        ompl.getProblem().goalState = [-1.5035205538442702, 0.8730168650583671, 1.6298590879018415, 1.7106630821349786, 0.8789956712153525, 0.12782224716566898, 0.0]
+        ompl.get_problem().start_state = [1.5035205538438838, 0.8730168650583787, -1.6298590879018438, 1.7106630821349438, -0.8789956712153559, 0.1278222471656531, 0.0]
+        ompl.get_problem().goal_state = [-1.5035205538442702, 0.8730168650583671, 1.6298590879018415, 1.7106630821349786, 0.8789956712153525, 0.12782224716566898, 0.0]
 
         # 1st solve call
         solution = None
@@ -29,10 +29,10 @@ class OMPLLockedBoundsCase(unittest.TestCase):
         self.assertTrue(solution is not None)
 
         # get and change current bounds
-        jl = ompl.getProblem().getScene().getSolver().getJointLimits()
+        jl = ompl.get_problem().get_scene().get_kinematic_tree().get_joint_limits()
         jl[0] += 1
-        ompl.getProblem().getScene().getSolver().setJointLimitsLower(jl[:,0])
-        ompl.getProblem().getScene().getSolver().setJointLimitsUpper(jl[:,1])
+        ompl.get_problem().get_scene().get_kinematic_tree().set_joint_limits_lower(jl[:,0])
+        ompl.get_problem().get_scene().get_kinematic_tree().set_joint_limits_upper(jl[:,1])
 
         # 3rd call with changed bounds
         solution = None

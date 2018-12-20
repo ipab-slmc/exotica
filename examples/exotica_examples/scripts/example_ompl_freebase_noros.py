a4d0ed116cde8b6f1db216b7aa41d0399fd431a0
Vladimir Ivan <v.ivan@ed.ac.uk>
2018-12-20 02:43:27 +0100

    [exotica_examples] Updated floating base examples

--- examples/exotica_examples/scripts/example_ompl_freebase_noros.py
+++ examples/exotica_examples/scripts/example_ompl_freebase_noros.py
@@ -8,12 +8,8 @@ from pyexotica.publish_trajectory import *
 solver = exo.Setup.load_solver(
     '{exotica_examples}/resources/configs/ompl_solver_demo_freebase.xml')
 
-# set planar base limits
-base_limits = np.array([[-10, 10], [-10, 10], [0, 2 * np.pi]])
-solver.get_problem().get_scene().get_solver().set_planar_base_limits_pos_xy_euler_z(base_limits[:, 0], base_limits[:, 1])
-
 print("joint limits:\n" +
-      str(solver.get_problem().get_scene().get_solver().get_joint_limits()))
+      str(solver.get_problem().get_scene().get_kinematic_tree().get_joint_limits()))
 
 solution = solver.solve()
 

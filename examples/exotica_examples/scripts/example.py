4899b2072b9da25f7eba53d0272321829ad49ef9
Vladimir Ivan <v.ivan@ed.ac.uk>
2018-12-21 02:04:39 +0100

    [exotica_examples] Renaming and code standardization

--- examples/exotica_examples/scripts/example.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import pyexotica as exo
-solver = exo.Setup.load_solver(
-    '{exotica_examples}/resources/configs/example.xml')
-print(solver.solve())
---
+++ exotica_examples/src/exotica_examples_py/__init__.py
@@ -0,0 +1,1 @@
+from target_marker import TargetMarker

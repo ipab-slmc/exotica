4458800eec1f111cb964ec6389bd34d7f54982b7
Wolfgang Merkt <w.merkt@gmail.com>
2019-01-02 10:20:39 +0000

    [exotica_core_task_maps] SmoothCollisionDistance: Update for core renaming

--- exotations/exotica_core_task_maps/include/exotica_core_task_maps/smooth_collision_distance.h
+++ exotations/exotica_core_task_maps/include/exotica_core_task_maps/smooth_collision_distance.h
@@ -33,7 +33,7 @@
 #ifndef EXOTICA_CORE_TASK_MAPS_SMOOTH_COLLISION_DISTANCE_H_
 #define EXOTICA_CORE_TASK_MAPS_SMOOTH_COLLISION_DISTANCE_H_
 
-#include <exotica/TaskMap.h>
+#include <exotica_core/task_map.h>
 #include <exotica_core_task_maps/SmoothCollisionDistanceInitializer.h>
 #include <algorithm>
 #include <cmath>

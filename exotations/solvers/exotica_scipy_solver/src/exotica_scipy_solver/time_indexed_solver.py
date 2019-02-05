d98344b5d5e2bde8790a6201f62514cef56b2e4d
Wolfgang Merkt <w.merkt@gmail.com>
2019-02-05 10:59:44 +0000

    [exotica_scipy_solver] Update example

--- exotations/solvers/exotica_scipy_solver/src/exotica_scipy_solver/time_indexed_solver.py
+++ exotations/solvers/exotica_scipy_solver/src/exotica_scipy_solver/time_indexed_solver.py
@@ -17,6 +17,7 @@ class SciPyTimeIndexedSolver(object):
         self.problem = problem
         self.debug = debug
         self.method = method
+        self.hessian_update_strategy = SR1()
     
     def specifyProblem(self, problem):
         self.problem = problem
@@ -27,52 +28,40 @@ class SciPyTimeIndexedSolver(object):
     
     def eq_constraint_jac(self, x):
         self.problem.update(x)
-        return self.problem.get_equality_jacobian
+        print("EQ", self.problem.get_equality_jacobian().shape)
+        return self.problem.get_equality_jacobian()
     
     def neq_constraint_fun(self, x):
         self.problem.update(x)
-        # print("NEQ", -1. * self.problem.get_inequality())
         return -1. * self.problem.get_inequality()
     
     def neq_constraint_jac(self, x):
         self.problem.update(x)
+        print("NEQ", self.problem.get_inequality_jacobian().shape)
         return -1. * self.problem.get_inequality_jacobian()
     
     def cost_fun(self, x):
-        for t in xrange(self.problem.T):
-            self.problem.update(x[t*self.problem.N:(t+1)*self.problem.N], t)
-        # cost = 0.0
-        # jac = np.zeros((self.problem.N * self.problem.T,))
-        # for t in xrange(self.problem.T):
-        #     cost += self.problem.get_scalar_task_cost(t) + self.problem.get_scalar_transition_cost(t)
+        self.problem.update(x)
         return self.problem.get_cost(), self.problem.get_cost_jacobian()
-    
-    def cost_jac(self, x):
-        for t in xrange(self.problem.T):
-            self.problem.update(x[t*self.problem.N:(t+1)*self.problem.N], t)
-        return self.problem.get_cost_jacobian()
     
     def solve(self):
         # Extract start state
-        x0 = self.problem.initial_trajectory
+        x0 = np.asarray(self.problem.initial_trajectory).flatten()
 
         # Add constraints
         cons = []
         if self.method != "trust-constr":
             if (self.problem.inequality.length_Phi > 0):
-                print("Adding NEQ")
                 cons.append({'type': 'ineq', 'fun': self.neq_constraint_fun, 'jac': self.neq_constraint_jac})
             
             if (self.problem.equality.length_Phi):
-                print("Adding EQ")
                 cons.append({'type': 'eq', 'fun': self.eq_constraint_fun, 'jac': self.eq_constraint_jac})
         else:
             if (self.problem.inequality.length_Phi > 0):
-                cons.append(NonlinearConstraint(self.neq_constraint_fun, 0., np.inf, jac=self.neq_constraint_jac, hess=SR1()))
+                cons.append(NonlinearConstraint(self.neq_constraint_fun, 0., np.inf, jac=self.neq_constraint_jac, hess=self.hessian_update_strategy))
             
             if (self.problem.equality.length_Phi):
-                cons.append(NonlinearConstraint(self.eq_constraint_fun, 0., 0., jac=self.eq_constraint_jac, hess=SR1()))
-            
+                cons.append(NonlinearConstraint(self.eq_constraint_fun, 0., 0., jac=self.eq_constraint_jac, hess=self.hessian_update_strategy))
 
         # Bounds
         bounds = None
@@ -82,10 +71,10 @@ class SciPyTimeIndexedSolver(object):
         s = time()
         res = minimize(self.cost_fun, 
                     x0,
-                    # method=self.method,
+                    method=self.method,
                     bounds=bounds,
                     jac=True,
-                    hess=SR1(),
+                    hess=self.hessian_update_strategy,
                     constraints=cons,
                     options={'disp': self.debug, 'initial_tr_radius':1000., 'verbose': 2})
         e = time()

15f09065c594e8e7121fd1aed0cccda621ae7943
Christian Rauch <Christian.Rauch@ed.ac.uk>
2019-05-10 18:10:31 +0100

    use default Instantiate implementation for IK and LevenbergMarquardt

--- exotations/solvers/exotica_ik_solver/src/ik_solver.cpp
+++ exotations/solvers/exotica_ik_solver/src/ik_solver.cpp
@@ -37,11 +37,6 @@ IKSolver::IKSolver() = default;
 
 IKSolver::~IKSolver() = default;
 
-void IKSolver::Instantiate(const IKSolverInitializer& init)
-{
-    parameters_ = init;
-}
-
 void IKSolver::SpecifyProblem(PlanningProblemPtr pointer)
 {
     if (pointer->type() != "exotica::UnconstrainedEndPoseProblem")

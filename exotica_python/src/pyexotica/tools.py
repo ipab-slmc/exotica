41511c783645d88e69e7388dd5610c61a54fe700
Wolfgang Merkt <w.merkt@gmail.com>
2019-07-16 00:08:13 +0100

    [exotica_python] Add get_colliding_links to tools

--- exotica_python/src/pyexotica/tools.py
+++ exotica_python/src/pyexotica/tools.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 from time import time
 
-__all__ = ["check_trajectory_continuous_time"]
+__all__ = ["check_trajectory_continuous_time", "get_colliding_links"]
 
 def check_trajectory_continuous_time(scene, trajectory):
     start_time = time()
@@ -26,3 +26,24 @@ def check_trajectory_continuous_time(scene, trajectory):
     end_time = time()
     print("Continuous-time collision verification took", end_time - start_time)
     return all_good
+
+def get_colliding_links(scene, margin=0.0, safe_distance=0.0, check_self_collision=True):
+    proxies = scene.get_collision_distance(True)
+    robotLinks = scene.get_collision_robot_links()
+    world_links = scene.get_collision_world_links()
+    for r_l in robotLinks:
+        for w_l in world_links:
+            if scene.is_allowed_to_collide(r_l, w_l, True):
+                if not scene.is_collision_free(r_l, w_l, margin):
+                    d=scene.get_collision_distance(r_l, w_l)
+                    if abs(d[0].distance) > safe_distance:
+                        print(r_l,"-",w_l,"d=",d[0].distance)
+        if check_self_collision:
+            for w_l in robotLinks:
+                if w_l != r_l:
+                    if scene.is_allowed_to_collide(r_l, w_l, True):
+                        if not scene.is_collision_free(r_l, w_l, margin):
+                            d=scene.get_collision_distance(r_l,w_l)
+                            if abs(d[0].distance) > safe_distance:
+                                print(r_l,"-",w_l,d[0].distance)
+

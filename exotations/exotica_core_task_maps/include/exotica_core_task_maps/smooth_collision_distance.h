2e8b766ccdd47ae34622d92a9d63fb61618e57a6
Wolfgang Merkt <w.merkt@gmail.com>
2020-02-12 20:40:02 +0000

    [exotica_core_task_maps] Modify to use GetRobotToRobotCollisionDistance, GetRobotToWorldCollisionDistance As this now uses AABB distance checking before performing a distance calculation, a lot fewer distances are actually computed using GJK/EPA resulting in orders of magnitude speed-ups.

--- exotations/exotica_core_task_maps/include/exotica_core_task_maps/smooth_collision_distance.h
+++ exotations/exotica_core_task_maps/include/exotica_core_task_maps/smooth_collision_distance.h
@@ -52,8 +52,6 @@ public:
 private:
     void Initialize();
 
-    std::vector<std::string> robot_joints_;
-    std::map<std::string, std::vector<std::string>> controlled_joint_to_collision_link_map_;
     double robot_margin_ = 0.0;
     double world_margin_ = 0.0;
     bool linear_ = false;

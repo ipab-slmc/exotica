e4ce1b1999d94c361af5ca5cdcd1b8e38453d3b9
Wolfgang Merkt <w.merkt@gmail.com>
2020-04-26 20:55:28 +0100

    [exotica_core] Printable: Add PrintDimensions for matrices/vectors

--- exotica_core/src/tools/printable.cpp
+++ exotica_core/src/tools/printable.cpp
@@ -55,4 +55,9 @@ std::string ToString(const Eigen::Affine3d& s)
     Eigen::Quaterniond quat(s.rotation());
     return "([" + std::to_string(s.translation().x()) + " " + std::to_string(s.translation().y()) + " " + std::to_string(s.translation().z()) + "] [" + std::to_string(quat.x()) + " " + std::to_string(quat.y()) + " " + std::to_string(quat.z()) + " " + std::to_string(quat.w()) + "])";
 }
+
+void PrintDimensions(const std::string& name, const Eigen::Ref<const Eigen::MatrixXd> m)
+{
+    INFO_NAMED(name, m.rows() << "x" << m.cols());
 }
+}  // namespace exotica

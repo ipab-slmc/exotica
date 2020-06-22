4b734ed5f2f149948f94451a8ac2f43ebdec403c
Wolfgang Merkt <w.merkt@gmail.com>
2020-06-23 00:24:31 +0100

    [exotica_python] test_no_unknown_initializer_types: Upgrade to support both Python2 and Python3

--- exotica_python/test/test_no_unknown_initializer_types.py
+++ exotica_python/test/test_no_unknown_initializer_types.py
@@ -1,8 +1,15 @@
 from __future__ import print_function
 import subprocess
+import os
 
-output = subprocess.check_output("python -c 'import pyexotica as exo ; initializers = exo.Setup.get_initializers()'", shell=True)
+output = None
+if os.environ['ROS_PYTHON_VERSION'] == '2':
+    output = subprocess.check_output("python -c 'import pyexotica as exo ; initializers = exo.Setup.get_initializers()'", shell=True)
+elif os.environ['ROS_PYTHON_VERSION'] == '3':
+    output = subprocess.check_output("python3 -c 'import pyexotica as exo ; initializers = exo.Setup.get_initializers()'", shell=True)
+else:
+    assert False
 
-if "Skipping" in output:
+if b"Skipping" in output:
     print(output)
-    assert "Skipping" not in output
+    assert b"Skipping" not in output

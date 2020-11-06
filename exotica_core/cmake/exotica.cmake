daf2882c1547ee68a670845138cfaff032777bd9
Wolfgang Merkt <w.merkt@gmail.com>
2020-11-06 02:23:48 +0000

    [exotica_core] Upgrade standard to C++14 for new pybind11 version

--- exotica_core/cmake/exotica.cmake
+++ exotica_core/cmake/exotica.cmake
@@ -1,7 +1,8 @@
 cmake_minimum_required(VERSION 3.0.2)
 
-message(STATUS "Compiling using c++ 11 (required by EXOTica)")
-add_compile_options(-std=c++11 -Wfloat-conversion -Wall -Wno-maybe-uninitialized)
+message(STATUS "Compiling using C++ 14 (required by EXOTica)")
+set(CMAKE_CXX_STANDARD 14)
+add_compile_options(-Wfloat-conversion -Wall -Wno-maybe-uninitialized)
 # add_compile_options(-Werror)
 
 # MoveIt Core Robot Model isnt aligned :'(

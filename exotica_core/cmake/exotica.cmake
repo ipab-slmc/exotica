415b35361bcbd1d933ef39a6a25d9ab82fb5f94a
Wolfgang Merkt <w.merkt@gmail.com>
2021-02-08 23:15:01 +0000

    [exotica_core] Remove maybe-unitialized
    This is gcc only.

--- exotica_core/cmake/exotica.cmake
+++ exotica_core/cmake/exotica.cmake
@@ -2,7 +2,8 @@ cmake_minimum_required(VERSION 3.0.2)
 
 message(STATUS "Compiling using C++ 14 (required by EXOTica)")
 set(CMAKE_CXX_STANDARD 14)
-add_compile_options(-Wfloat-conversion -Wall -Wno-maybe-uninitialized)
+add_compile_options(-Wfloat-conversion -Wall)
+# gcc only: -Wno-maybe-uninitialized
 # add_compile_options(-Werror)
 
 # MoveIt Core Robot Model isnt aligned :'(

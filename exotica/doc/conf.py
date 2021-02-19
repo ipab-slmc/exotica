fd753148cd2623142ceb2a04758de4d5b3e7d688
Wolfgang Merkt <w.merkt@gmail.com>
2021-02-19 14:41:36 +0000

    [exotica] Adjust version for documentation

--- exotica/doc/conf.py
+++ exotica/doc/conf.py
@@ -19,9 +19,9 @@ copyright = u'2020, Vladimir Ivan, Wolfgang Merkt, Sethu Vijayakumar - and contr
 author = u'Vladimir Ivan, Wolfgang Merkt, Sethu Vijayakumar - and contributors'
 
 # The short X.Y version.
-version = u'6.0.0'
+version = u'6.0.2'
 # The full version, including alpha/beta/rc tags.
-release = u'6.0.0'
+release = u'6.0.2'
 
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'Doxyfile']

7fbefcb86a35e61bdbeb6c4f397a1fb230cd90fe
Wolfgang Merkt <w.merkt@gmail.com>
2022-01-21 13:13:25 +0000

    [exotica_python] Add unittest for mesh creation

---
+++ exotica_python/test/test_mesh.py
@@ -0,0 +1,46 @@
+import unittest
+
+import pyexotica as exo
+import trimesh
+
+def test_mesh(mesh):
+    print(mesh, mesh.vertex_count, mesh.triangle_count)
+    assert mesh.vertex_count == 33
+    assert mesh.triangle_count == 62
+
+class TestPythonMeshCreation(unittest.TestCase):
+    def test_create_mesh_from_resource_package_path(self):
+        # Load mesh from resource path
+        print(">>> Loading STL directly from package-path")
+        mesh = exo.Mesh.createMeshFromResource("package://exotica_examples/resources/cone.stl")
+        test_mesh(mesh)
+
+    def test_create_mesh_from_resource_exotica_resource_path(self):
+        # Load STL mesh from Exotica resource path
+        print(">>> Loading STL from Exotica-resource-path")
+        mesh = exo.Mesh.createMeshFromResource("{exotica_examples}/resources/cone.stl")
+        test_mesh(mesh)
+
+        # Load OBJ from Exotica resource path
+        print(">>> Loading OBJ from Exotica-resource-path")
+        mesh = exo.Mesh.createMeshFromResource("{exotica_examples}/resources/cone.obj")
+        test_mesh(mesh)
+
+    def test_create_mesh_from_vertices(self):
+        # Load mesh from vertices and triangles
+        print(">>> Creating mesh from list of vertices")
+        # Note: exotica's load_obj is not compatible with all obj files, so don't use it.
+        m = trimesh.load(exo.Tools.parse_path("{exotica_examples}/resources/cone.obj"))
+        vertices = [m.vertices[i] for i in m.faces.flatten()]
+        mesh = exo.Mesh.createMeshFromVertices(vertices)
+        test_mesh(mesh)
+
+    def teset_create_mesh_from_vertices_and_triangles(self):
+        # STL
+        print(">>> Creating mesh from list of vertices and list of triangles")
+        m = trimesh.load(exo.Tools.parse_path("{exotica_examples}/resources/cone.stl"))
+        mesh = exo.Mesh.createMeshFromVertices(m.vertices, m.faces.flatten())
+        test_mesh(mesh)
+
+if __name__ == "__main__":
+    unittest.main()

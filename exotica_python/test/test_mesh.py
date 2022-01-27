a25380d07f7234b4528d1f8da81511d3e6266e63
Wolfgang Merkt <w.merkt@gmail.com>
2022-01-27 12:55:06 +0000

    [exotica_python] Rename function from test_ to validate_ to avoid triggering nosetests
    Nosetests runs any function with "test_" in the name. The validation
    function should of course be excluded.

--- exotica_python/test/test_mesh.py
+++ exotica_python/test/test_mesh.py
@@ -3,7 +3,7 @@ import unittest
 import pyexotica as exo
 import trimesh
 
-def test_mesh(mesh):
+def validate_mesh(mesh):
     print(mesh, mesh.vertex_count, mesh.triangle_count)
     assert mesh.vertex_count == 33
     assert mesh.triangle_count == 62
@@ -13,18 +13,18 @@ class TestPythonMeshCreation(unittest.TestCase):
         # Load mesh from resource path
         print(">>> Loading STL directly from package-path")
         mesh = exo.Mesh.createMeshFromResource("package://exotica_examples/resources/cone.stl")
-        test_mesh(mesh)
+        validate_mesh(mesh)
 
     def test_create_mesh_from_resource_exotica_resource_path(self):
         # Load STL mesh from Exotica resource path
         print(">>> Loading STL from Exotica-resource-path")
         mesh = exo.Mesh.createMeshFromResource("{exotica_examples}/resources/cone.stl")
-        test_mesh(mesh)
+        validate_mesh(mesh)
 
         # Load OBJ from Exotica resource path
         print(">>> Loading OBJ from Exotica-resource-path")
         mesh = exo.Mesh.createMeshFromResource("{exotica_examples}/resources/cone.obj")
-        test_mesh(mesh)
+        validate_mesh(mesh)
 
     def test_create_mesh_from_vertices(self):
         # Load mesh from vertices and triangles
@@ -33,14 +33,14 @@ class TestPythonMeshCreation(unittest.TestCase):
         m = trimesh.load(exo.Tools.parse_path("{exotica_examples}/resources/cone.obj"))
         vertices = [m.vertices[i] for i in m.faces.flatten()]
         mesh = exo.Mesh.createMeshFromVertices(vertices)
-        test_mesh(mesh)
+        validate_mesh(mesh)
 
     def teset_create_mesh_from_vertices_and_triangles(self):
         # STL
         print(">>> Creating mesh from list of vertices and list of triangles")
         m = trimesh.load(exo.Tools.parse_path("{exotica_examples}/resources/cone.stl"))
         mesh = exo.Mesh.createMeshFromVertices(m.vertices, m.faces.flatten())
-        test_mesh(mesh)
+        validate_mesh(mesh)
 
 if __name__ == "__main__":
     unittest.main()
